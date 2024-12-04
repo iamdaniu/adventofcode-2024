655150c266108d82eaf548163d67e529413be41a
joern <joernspam@gmx.de>
2024-12-04 12:30:25 +0100

    day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -15,20 +15,39 @@ def is_out_of_bounds(lines: list, point: tuple):
         return True
     return False
 
-def apply_search_mode(point: tuple, search_mode: tuple):
-    return(point[0] + search_mode[0], point[1] + search_mode[1])
-
-def neighboring_search_modes(search_mode: tuple):
-    return ()
-
-def has_neighboring_m(lines: list, center_y: int, center_x: int, search_mode: tuple):
-    return False
-
 def character_matches(lines: list, check_for: str, point: tuple):
     if is_out_of_bounds(lines, point):
         return False
     character = lines[point[0]][point[1]]
     return character == check_for
+
+def apply_search_mode(point: tuple, search_mode: tuple):
+    return(point[0] + search_mode[0], point[1] + search_mode[1])
+
+def opposite_search_mode(search_mode: tuple):
+    return (-search_mode[0], -search_mode[1])
+
+def neighboring_search_modes(search_mode: tuple):
+    top_left = (search_mode[0], -search_mode[1])
+    bottom_right = (-search_mode[0], search_mode[1])
+    return (top_left, bottom_right)
+
+def has_neighboring_m(lines: list, point: tuple, search_mode: tuple):
+    return False
+
+def m_and_opposite_s(lines: list, center: tuple, search_mode: tuple):
+    m = character_matches(lines, "M", apply_search_mode(center, search_mode))
+    s = character_matches(lines, "S", apply_search_mode(center, opposite_search_mode(search_mode)))
+    return m and s
+
+def find_x_mas(lines: list, center: tuple):
+    if character_matches(lines, "A", center):
+        for corner in [(-1, -1), (1, 1), (-1, 1), (1, -1)]:
+            if m_and_opposite_s(lines, center, corner):
+                for neighbor in neighboring_search_modes(corner):
+                    if m_and_opposite_s(lines, center, neighbor):
+                        return True
+    return False
 
 def find_in_direction(lines: list, point: tuple, index: int, search_mode: tuple):
     if character_matches(lines, search_term[index], point):
@@ -51,9 +70,12 @@ with open('day4/data.txt', newline='') as input_file:
     lines = input_file.readlines()
 
 xmas_count = 0
+cross_mas_count = 0
 for y in range(0, len(lines)):
     line = lines[y]
     for x in range(0, len(line)):
         xmas_count = xmas_count + find_from(lines, (y, x))
+        cross_mas_count = cross_mas_count + find_x_mas(lines, (y, x))
 
-print(f"{xmas_count} matches found")
+print(f"{xmas_count} XMAS found")
+print(f"{cross_mas_count} cross-MAS found")
