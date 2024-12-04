134853ab592b054536005c4180f571e9a9593a38
joern <joernspam@gmx.de>
2024-12-04 12:10:21 +0100

    refactoring to point

--- day4/day4.py
+++ day4/day4.py
@@ -5,29 +5,46 @@ search_modes = search_modes + [(1, -1), (1, 0), (1, 1)]
 
 search_term = "XMAS"
 
-def find_in_direction(lines: list, cy: int, cx: int, index: int, search_mode: tuple):
+def is_out_of_bounds(lines: list, point: tuple):
+    y = point[0]
+    x = point[1]
     # out of bounds checks
-    if cy < 0 or cx < 0:
-        return 0
-    if cy >= len(lines) or cx >= len(lines[cy]):
-        return 0
-    character = lines[cy][cx]
-    toFind = search_term[index]
+    if y < 0 or x < 0:
+        return True
+    if y >= len(lines) or x >= len(lines[y]):
+        return True
+    return False
+
+def apply_search_mode(point: tuple, search_mode: tuple):
+    return(point[0] + search_mode[0], point[1] + search_mode[1])
+
+def neighboring_search_modes(search_mode: tuple):
+    return ()
+
+def has_neighboring_m(lines: list, center_y: int, center_x: int, search_mode: tuple):
+    return False
+
+def character_matches(lines: list, check_for: str, point: tuple):
+    if is_out_of_bounds(lines, point):
+        return False
+    character = lines[point[0]][point[1]]
+    return character == check_for
+
+def find_in_direction(lines: list, point: tuple, index: int, search_mode: tuple):
+    if character_matches(lines, search_term[index], point):
     # current position matches expected character - continue search
-    if character == toFind:
         new_index = index + 1
         if new_index == len(search_term):
             return 1
-        new_cy = cy + search_mode[0]
-        new_cx = cx + search_mode[1]
-        return find_in_direction(lines, new_cy, new_cx, new_index, search_mode)
+        new_candidate = apply_search_mode(point, search_mode)
+        return find_in_direction(lines, new_candidate, new_index, search_mode)
     # current character mismatch
     return 0
 
-def find_from(lines: list, y: int, x: int):
+def find_from(lines: list, point: tuple):
     result = 0
     for mode in search_modes:
-        result = result + find_in_direction(lines, y, x, 0, mode)
+        result = result + find_in_direction(lines, point, 0, mode)
     return result
 
 with open('day4/data.txt', newline='') as input_file:
@@ -37,6 +54,6 @@ xmas_count = 0
 for y in range(0, len(lines)):
     line = lines[y]
     for x in range(0, len(line)):
-        xmas_count = xmas_count + find_from(lines, y, x)
+        xmas_count = xmas_count + find_from(lines, (y, x))
 
 print(f"{xmas_count} matches found")
