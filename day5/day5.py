9c54948af11b3d29e75131c77713fbe4d46d2649
joern <joernspam@gmx.de>
2024-12-05 13:16:33 +0100

    day 5 part 2

--- day5/day5.py
+++ day5/day5.py
@@ -4,23 +4,25 @@ from functools import cmp_to_key
 
 order_pattern = re.compile("(\d+)\|(\d+)")
 
+orderings = {}
 
 def read_order(line: str):
     match = order_pattern.match(line)
     if match != None:
-        return (int(match.group(1)), int(match.group(2)))
+        left = int(match.group(1))
+        right = int(match.group(2))
+        after_list = orderings.get(left)
+        if after_list:
+            after_list.append(right)
+        else:
+            orderings[left] = [right]
+        return True
     else:
-        return None
-
-orderings = {}
+        return False
 
 def required_after(p1: int, p2: int):
     order_after = orderings.get(p1)
-    if order_after:
-        if p2 in order_after:
-            return True
-    return False
-
+    return p2 in order_after
 
 def compare_by_ordering(p1: int, p2: int):
     if required_after(p1, p2):
@@ -30,28 +32,22 @@ def compare_by_ordering(p1: int, p2: int):
     else:
         return 0
 
-
-center_value_sum = 0
+correct_value_sum = 0
+incorrect_value_sum = 0
 
 with open("day5/data.txt") as input_file:
     reading_order = True
     for line in input_file.readlines():
-        if (reading_order):
-            new_order = read_order(line)
-            if new_order:
-                after_list = orderings.get(new_order[0])
-                if after_list:
-                    after_list.append(new_order[1])
-                else:
-                    orderings[new_order[0]] = [new_order[1]]
-            else:
-                reading_order = False
+        if reading_order:
+            reading_order = read_order(line)
         else:
             pages = [int(page) for page in re.split(",", line)]
             resorted = sorted(pages, key=cmp_to_key(compare_by_ordering))
+            middle_value = resorted[int(len(resorted)/2)]
             if pages == resorted:
-                middle_value = pages[int(len(pages)/2)]
-                print(f"correctly sorted: {pages}, center: {middle_value}")
-                center_value_sum = center_value_sum + middle_value
+                correct_value_sum = correct_value_sum + middle_value
+            else:
+                incorrect_value_sum = incorrect_value_sum + middle_value
 
-print(f"result: {center_value_sum}")
+print(f"result correctly sorted: {correct_value_sum}")
+print(f"result incorrectly sorted: {incorrect_value_sum}")
