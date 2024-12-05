28b60cdcccef2da08e6d00464bd302f9336e8402
joern <joernspam@gmx.de>
2024-12-05 13:35:06 +0100

    refactoring sorter to class

--- day5/day5.py
+++ day5/day5.py
@@ -4,41 +4,48 @@ from functools import cmp_to_key
 
 order_pattern = re.compile("(\d+)\|(\d+)")
 
-orderings = {}
+class OrderingSorter:
+    def __init__(self):
+        self.orderings = {}
 
-def read_order(line: str):
-    match = order_pattern.match(line)
-    if match != None:
-        left = int(match.group(1))
-        right = int(match.group(2))
-        after_list = orderings.get(left)
+    def add_order(self, left: int, right: int):
+        after_list = self.orderings.get(left)
         if after_list:
             after_list.append(right)
         else:
-            orderings[left] = [right]
-        return True
-    else:
-        return False
+            self.orderings[left] = [right]
 
-def compare_by_ordering(p1: int, p2: int):
-    if p2 in orderings.get(p1):
-        return -1
-    elif p1 in orderings.get(p2):
-        return 1
-    else:
-        return 0
+    def is_after(self, p1, p2):
+        after_list = self.orderings.get(p1)
+        return after_list and p2 in after_list
+
+    def compare_by_ordering(self, p1: int, p2: int):
+        if self.is_after(p1, p2):
+            return -1
+        elif self.is_after(p2, p1):
+            return 1
+        else:
+            return 0
+
 
 correct_value_sum = 0
 incorrect_value_sum = 0
 
-with open("day5/data.txt") as input_file:
+sorter = OrderingSorter()
+
+with open("day5/sample.txt") as input_file:
     reading_order = True
     for line in input_file.readlines():
+        if line == "\n":
+            reading_order = False
+            continue
+
         if reading_order:
-            reading_order = read_order(line)
+            match = order_pattern.match(line)
+            sorter.add_order(int(match.group(1)), int(match.group(2)))
         else:
             pages = [int(page) for page in re.split(",", line)]
-            resorted = sorted(pages, key=cmp_to_key(compare_by_ordering))
+            resorted = sorted(pages, key=cmp_to_key(sorter.compare_by_ordering))
             middle_value = resorted[int(len(resorted)/2)]
             if pages == resorted:
                 correct_value_sum = correct_value_sum + middle_value
