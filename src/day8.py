cfccf46279afe8a0da0d43dab091a5d0db1e6f6f
joern <joernspam@gmx.de>
2024-12-08 12:16:42 +0100

    day 8 part 2

--- src/day8.py
+++ src/day8.py
@@ -1,3 +1,6 @@
+from typing import Callable
+#from typing import Generator
+
 from map_lib import Point
 from map_lib import Map
 from map_lib import print_map
@@ -9,9 +12,18 @@ def read_map(filename: str) -> Map:
             result.add_line(line)
     return result
 
+def generate_candidates(start_point: Point, diff: Point, op: Callable[[int, int], int]):
+    current_point = Point(op(start_point.y, diff.y), op(start_point.x, diff.x))
+    while True:
+        yield current_point
+        current_point = Point(op(current_point.y, diff.y), op(current_point.x, diff.x))
+
+
 def antinodes(tower_map: Map, frequency: str) -> set[Point]:
     points = tower_map.points_with_interest(frequency)
-    result = set()
+    if (len(points) == 1):
+        return set()
+    result = set(points)
     for first in range(len(points)-1):
         point_1 = points[first]
         for second in range(first+1, len(points)):
@@ -19,16 +31,25 @@ def antinodes(tower_map: Map, frequency: str) -> set[Point]:
             diff = Point(point_1.y - point_2.y, point_1.x - point_2.x)
 
             #print(f"checking points {point_1} and {point_2}, diff {diff}")
-            for candidate in [Point(point_1.y + diff.y, point_1.x + diff.x), Point(point_2.y - diff.y, point_2.x - diff.x) ]:
+            #for candidate in [Point(point_1.y + diff.y, point_1.x + diff.x), Point(point_2.y - diff.y, point_2.x - diff.x) ]:
+            for candidate in generate_candidates(point_1, diff, lambda x, y: x + y):
                 if (not tower_map.out_of_bounds(candidate)):
                     #print(f"antinode found: {candidate}")
                     result.add(candidate)
                     tower_map.mark(candidate, "#")
+                else:
+                    break
+            for candidate in generate_candidates(point_2, diff, lambda x, y: x - y):
+                if (not tower_map.out_of_bounds(candidate)):
+                    #print(f"antinode found: {candidate}")
+                    result.add(candidate)
+                    tower_map.mark(candidate, "#")
+                else:
+                    break
                 # else:
                 #     print("out of bounds")
     #print(f"antinodes for {frequency}: {points}")
     return result
-
 
 
 def main():
@@ -39,7 +60,7 @@ def main():
         frequency_antinodes = antinodes(tower_map, frequency)
         all_antinodes.update(frequency_antinodes)
     print(f"{len(all_antinodes)} antinodes: {all_antinodes}")
-    #print_map(tower_map)
+    print_map(tower_map)
 
 
 if __name__ == '__main__':
